c8bafaee1f90ed271ae59b538dea64a2522499bf
ProValdi <prokhorov.va@phystech.edu>
2021-08-13 14:42:49 +0000

    API definition transferred by SwaggerHub

---
+++ server/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.nums import Nums
+from swagger_server.models.results import Results
