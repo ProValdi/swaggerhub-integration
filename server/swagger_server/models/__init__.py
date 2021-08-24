a094afc621da9a52d99b6c46f07820522a072814
ProValdi <prokhorov.va@phystech.edu>
2021-08-24 13:30:17 +0000

    Push from SwaggerHub.

--- server/swagger_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.nums import Nums
-from swagger_server.models.results import Results
